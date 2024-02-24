f15b5a8b7b1d2d05492f7e32e29c5ef25b1edfb1
GabrielFilomeno <biel_floripa@hotmail.com>
2024-02-24 20:06:43 -0300

    Revert "Update script.js"
    This reverts commit 6ab0249e353a8df75b1b84c1d3e00e59a84538c6.

--- Snake-Game/script.js
+++ Snake-Game/script.js
@@ -59,7 +59,6 @@ function createsnakeHead() {
 function createCalda() {
 
     let lastIndex = snakeHead.length -1;
-    let penultimoIndex = snakeHead.length -2;
 
     var imgCalda = new Image();
 
